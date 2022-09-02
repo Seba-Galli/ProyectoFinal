198e1498a6860121167cd9c6b8b82e2d1f97137a
Seba-Galli <sebastianegalli@gmail.com>
2022-09-02 20:27:26 -0300

    mejorando el template

--- MiApp/urls.py
+++ MiApp/urls.py
@@ -3,7 +3,7 @@ from MiApp.views import entrada, estacionamiento, salida, inicio
 
 urlpatterns = [
     path('', inicio),
-    path('estacionamiento/', estacionamiento),
-    path('entrada/', entrada),
-    path('salida/', salida),
+    path('estacionamiento/', estacionamiento, name='AppEstacionamiento'),
+    path('entrada/', entrada, name='AppEntrada'),
+    path('salida/', salida, name='AppSalida'),
 ]
