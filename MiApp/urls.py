6b7d9c43c625cd1812032e23cb55f4023d99b2ca
Seba-Galli <sebastianegalli@gmail.com>
2022-09-04 19:59:54 -0300

    Herencia de templates

--- MiApp/urls.py
+++ MiApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from MiApp.views import entrada, estacionamiento, salida, inicio
 
 urlpatterns = [
-    path('', inicio),
+    path('', inicio, name='MiAppInicio'),
     path('estacionamiento/', estacionamiento, name='AppEstacionamiento'),
     path('entrada/', entrada, name='AppEntrada'),
     path('salida/', salida, name='AppSalida'),
