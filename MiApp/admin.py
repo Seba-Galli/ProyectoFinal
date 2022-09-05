fccbc81a719f86852f33a784e1247cbf490115f6
Seba-Galli <sebastianegalli@gmail.com>
2022-09-04 21:02:49 -0300

    alta de panel y creacion de usuario

--- MiApp/admin.py
+++ MiApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from MiApp.models import Estacionamiento, Entrada, Salida
 
-# Register your models here.
+admin.site.register(Estacionamiento)
+admin.site.register(Entrada)
+admin.site.register(Salida)
